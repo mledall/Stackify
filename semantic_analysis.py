0d13a90be2700660f95ed1cd7aa221877705bf3f
mledall <matthias.ledall08@gmail.com>
2018-09-29 23:30:07 -0400

    generated pca clustering plots on words and questions.

--- semantic_analysis.py
+++ semantic_analysis.py
@@ -1,14 +1,68 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import pickle
+from sklearn.decomposition import PCA
 
-from Data_processing import semantic_distance_to_other
+import seaborn as sns
 
 data = pickle.load(open('Stack_api_data_processed.pkl', "rb" ))
-#model = pickle.load(open("word2vec_model.pkl", "rb" ))
 
-print(semantic_distance_to_other(data.loc[1, 'relevant keywords'][0][0]))
-#print(data.loc[:, 'relevant keywords'][0][0])
+def field_clustering():
+	X = list(data.loc[:,'w2v'])
+	labels = data.loc[:, 'field']
+	pca = PCA(n_components=2)
+	Ypca = pca.fit_transform(X)
 
-#semantic_distance_to_other(keyword)
+	PCAdata = pd.concat([pd.DataFrame(Ypca, columns = ['x', 'y']), labels], axis = 1)
+	sns.lmplot(x='x', y='y', data=PCAdata, fit_reg=False, hue='field')
+	plt.show()
+#field_clustering()
+
+def word_clustering():
+	X = list(data.loc[:,'relevant keywords w2v'])
+	v = []
+	question_label = []
+	for i in range(len(data)):
+		x = X[i]
+		for y in x:
+			v.append(y)
+			question_label.append(data.loc[i,'question_id'])
+	labels = pd.DataFrame(question_label, columns = ['question'])
+	pca = PCA(n_components=2)
+	Ypca = pca.fit_transform(v)
+
+	PCAdata = pd.concat([pd.DataFrame(Ypca, columns = ['x', 'y']), labels], axis = 1)
+	sns.lmplot(x='x', y='y', data=PCAdata, fit_reg=False, hue = 'question')
+	plt.show()
+
+def word_clustering():
+	X = data.loc[:,'relevant keywords w2v']
+	v = []
+	question_label = []
+	for i in range(len(X)):
+		x = X[i]
+		for y in x:
+			v.append(y)
+			question_label.append(data.loc[i,'question_id'])
+	labels = pd.DataFrame(question_label, columns = ['question'])
+	pca = PCA(n_components=2)
+	Ypca = pca.fit_transform(v)
+
+	PCAdata = pd.concat([pd.DataFrame(Ypca, columns = ['x', 'y']), labels], axis = 1)
+	sns.lmplot(x='x', y='y', data=PCAdata, fit_reg=False, hue = 'question', legend = False)
+	plt.show()
+
+word_clustering()
+
+
+#print(pca.explained_variance_ratio_)  
+#print(pca.singular_values_)  
+
+
+
+
+
+
 
 
