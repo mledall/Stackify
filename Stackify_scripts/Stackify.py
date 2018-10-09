c4e704fe21120c5cfc4df0f243cf339908e00c33
mledall <matthias.ledall08@gmail.com>
2018-10-09 14:10:44 -0400

    working pipeline

---
+++ Stackify_scripts/Stackify.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from Stackify_pipeline import main_function
+
+link = 'https://stackoverflow.com/questions/3496592/conditional-import-of-modules-in-python'
+link2 = 'https://biology.stackexchange.com/questions/54690/what-is-the-role-of-tracrrna-in-crispr-cas9'
+link3 = 'https://physics.stackexchange.com/questions/251867/what-manifold-is-spacetime'
+
+main_function(link3)
+
+
+
