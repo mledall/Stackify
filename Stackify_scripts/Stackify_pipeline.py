7278a423ff4d1e3063ac63cedfc66863df99a051
mledall <matthias.ledall08@gmail.com>
2018-10-31 12:45:47 -0400

    added comments

--- Stackify_scripts/Stackify_pipeline.py
+++ Stackify_scripts/Stackify_pipeline.py
@@ -1,12 +1,15 @@
 '''
-This is going to be the pipeline of the stackify app
+This scripts is the pipeline for the Stackify app
 
-1) I get the text
-2) I scrape the keywords
-3) I order them in order of semantic relevance
-4) I link to another article in relevance order
+1) Get the text that the user is reading
+2) Extract the keywords from the text, along with their TextRank score and word average
+3) For all of the keywords, fetch the 3 most relevant other articles based on the semantic distance between the word vectors and the question average vectors
+4) Fetch the link for all of the vectors.
+
+Because of the limitation of the app, currently I am unable to apply this pipeline to any StackExchange post. This is because I built a question database ahead of time and compare articles within that data base. 
+
+To address this limitation, I need to scrape any StackExchange and apply the app to that new text. Doing so will allow me to be more general than just StackExchange.
 '''
-
 
 import pandas as pd
 import pickle
@@ -17,6 +20,9 @@ data = pickle.load(open('iterative_scraping_Stackoverflow_processed.pkl', "rb" )
 raw_data = pickle.load(open('iterative_scraping_Stackoverflow.pkl', "rb" ))
 
 def user_input(link):
+	'''
+	summarizes the information from the article being read by the user. The input link must belong to the database, this is a big limitation.
+	'''
 	index = list(raw_data[raw_data.loc[:, 'link'] == link].index)[0]
 	question_id = raw_data.loc[index,'id']
 	title = BeautifulSoup(raw_data.loc[index,'title'], 'html.parser').get_text()
@@ -25,10 +31,16 @@ def user_input(link):
 	return index, question_id, title, body, field
 
 def semantic_distance(vec1, vec2):
+	'''
+	calculates a semantic distance based on the cosine similarity between vectors
+	'''
     cosine_similarity = np.dot(vec1, vec2)/(np.linalg.norm(vec1)* np.linalg.norm(vec2))
     return cosine_similarity
 
 def most_relevant_article(keyword_vec, data):
+	'''
+	creates a list of the most relevant questions given a specific word vector
+	'''
 	dummy_list = []
 	for i in range(len(data)):
 		w2v_average = data.loc[i, 'average w2v']
@@ -38,6 +50,9 @@ def most_relevant_article(keyword_vec, data):
 	return sorted(dummy_list, key=lambda distance: distance[-1], reverse = True)[:3]
 
 def keyword_vectors(index):
+	'''
+	get the vectors for each keyword from the database
+	'''
 	keyword_dict = {}
 	for word in data.loc[index, 'keywords w2v']:
 		keyword = word[0]
@@ -59,6 +74,9 @@ def semantic_distance_to_other(question_id, field):
 	return keyword_article_distance
 
 def main_function(link):
+	'''
+	Outputs the results in a formatted form
+	'''
 	index, question_id, title, body, field = user_input(link)
 	relevant_keywords = semantic_distance_to_other(question_id, field)
 	for key in relevant_keywords:
