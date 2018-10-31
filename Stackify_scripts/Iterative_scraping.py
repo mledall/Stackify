94d23f88103d5a2616d27e453a64fbbcea00c0bb
mledall <matthias.ledall08@gmail.com>
2018-10-31 12:02:43 -0400

    added comments

--- Stackify_scripts/Iterative_scraping.py
+++ Stackify_scripts/Iterative_scraping.py
@@ -1,3 +1,15 @@
+'''
+This script generates requests url to query the StackExchange web API. The queries include the following information:
+- question title
+- question body
+- question tags
+- question ID
+- question URL
+
+The script initially creates a data base by scraping the website of all articles published within a window of 5 weeks. Then iterate by shifting the window to the past, and keeps iterating the scraping of successive 5 week windows.
+
+'''
+
 import requests
 import json
 import pandas as pd
@@ -5,6 +17,9 @@ import pickle
 import time
 
 def scraper(baseurl, key, query, item_type, field, cols, pickle_path):
+	'''
+	Sends one request, parses the response and places it in a pickled dataframe
+	'''
 	response = requests.get(baseurl+query+key)
 	data_json = response.json()
 	items = []
@@ -16,6 +31,9 @@ def scraper(baseurl, key, query, item_type, field, cols, pickle_path):
 	return items_df
 
 def extend_data_base(baseurl, key, query, item_type, field, cols, pickle_path):
+	'''
+	add new responses to the database already created: allows to iteratively scraping the website and to keep adding to the data base. If the scraping is interrupted, no data is lost.
+	'''
 	raw_data = pickle.load(open(pickle_path, "rb" ))
 	new_items_df = scraper(baseurl, key, query, item_type, field, cols, pickle_path)
 	items_df = pd.concat([raw_data, new_items_df], axis = 0, ignore_index = True)
@@ -23,6 +41,9 @@ def extend_data_base(baseurl, key, query, item_type, field, cols, pickle_path):
 	return items_df
 
 def question_or_answer(week_init, week_fin, item_type, field):
+	'''
+	generates specific request urls
+	'''
 	if item_type == 'questions':
 		if field == 'python':
 			return '/2.2/questions?pagesize=100&fromdate='+str(week_init)+'&todate='+str(week_fin)+'&order=desc&sort=votes&tagged=python&site=stackoverflow&filter=!LR0Y.(TGRRDGqDYvGAwl1e'
@@ -41,6 +62,9 @@ def question_or_answer(week_init, week_fin, item_type, field):
 
 
 def data_base_iterator(week_fin, pickle_path, item_type, field):
+	'''
+	scrapes a window of 5 weeks
+	'''
 	day = 24*3600
 	week = day*7
 	key = '&key=J3NfKzbKZxySqX5YDFuKJw(('
@@ -58,6 +82,9 @@ def data_base_iterator(week_fin, pickle_path, item_type, field):
 	pickle.dump(week_fin, open('scraping_summary.pkl', "wb" ) )
 
 def main_scraping_function():
+	'''
+	iterates the scraping by shifting the time window
+	'''
 	day = 24*3600
 	week = day*7
 #	fromdate = 1372636800
@@ -77,6 +104,9 @@ def main_scraping_function():
 #	print(raw_data.loc[:,'field'])
 
 def initial_scraping():
+	'''
+	performs the scraping of the initial 5 week window to initialize the data base
+	'''
 	day = 24*3600
 	week = day*7
 	todate = 1538352000
